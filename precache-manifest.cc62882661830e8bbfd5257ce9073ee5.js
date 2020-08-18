56e12683fc48a6e1967e6bc1ad414333de6c1a9c
Anteneh Negera <angias.forever@gmail.com>
2020-08-18 15:12:34 -0600

    Updates

---
+++ precache-manifest.cc62882661830e8bbfd5257ce9073ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f594192852bcf8516e2fe656d184c084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7268eb1e11b8612dfcad",
+    "url": "/static/css/main.7cafcd3f.chunk.css"
+  },
+  {
+    "revision": "62ab58aacbeed4b00bb5",
+    "url": "/static/js/2.dd950a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd950a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7268eb1e11b8612dfcad",
+    "url": "/static/js/main.762d3bd1.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
